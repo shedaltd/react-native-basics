b111942d518e971b1d9ab8efcba20227a68077fa
Benedict Hobart <hobart.benedict@gmail.com>
2015-11-09 18:27:12 +1100

    fix: delete lesson 6 compo

--- lessons/6_animations/index.js
+++ lessons/6_animations/index.js
@@ -3,18 +3,9 @@ const {View, Text, TouchableOpacity, StyleSheet} = React;
 const ReactMotion = require('react-motion/native');
 const {Motion, spring} = ReactMotion;
 const Animations = React.createClass({
-    getInitialState: () => ({location: {
-        x: 0,
-        y: 0
-    }),
     render() {
         return (<View style={styles.container}>
             {/*TODO: Add a onPress listener and animate the box to the location of the touch using the transform style*/}
-            <Motion style={{x: spring(this.state.location.x), y: spring(this.state.location.y)}}>
-                {({translateX, translateY}) => (
-                    <View style={[styles.box/*, {transform: []}]*/}/>
-                )}
-            </Motion>
         </View>);
     }
 });
